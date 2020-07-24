1209a1953941e5465eb2027b625e1bfd790ff3b8
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2020-07-24 11:18:23 +0300

    RecapProject
    c#, entity framevork, sql, asp.net

---
+++ RecapProject1/RecapProject1/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecapProject1.Entities;
+
+namespace RecapProject1
+{
+    class NorthwindContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } //elimdeki product veri tabanındaki products a karşılık geliyor
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
